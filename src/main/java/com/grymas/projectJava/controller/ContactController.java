c8c86d2dd462fd835fd2247d92460e33d458661e
31grudnia <mikolaj.starczewski@gmail.com>
2022-10-31 00:34:54 +0100

    ver. 0.3.3 - added messages.html and modified

--- src/main/java/com/grymas/projectJava/controller/ContactController.java
+++ src/main/java/com/grymas/projectJava/controller/ContactController.java
@@ -4,6 +4,7 @@ import com.grymas.projectJava.model.Contact;
 import com.grymas.projectJava.service.ContactService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -15,6 +16,9 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Slf4j      // creates log object by lombok
@@ -53,6 +57,20 @@ public class ContactController {
 
         contactService.saveMessageDetails(contact);
         return "redirect:/contact"; //return new contact form
+    }
+
+    @RequestMapping("/displayMessages")
+    public ModelAndView displayMessages(Model model) {
+        List<Contact> contactMsgs = contactService.findMsgsWithOpenStatus();
+        ModelAndView modelAndView = new ModelAndView("messages.html");
+        modelAndView.addObject("contactMsgs", contactMsgs);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/closeMsg", method = GET)
+    public String closeMsg(@RequestParam int id, Authentication authentication) {
+        contactService.updateMsgStatus(id, authentication.getName());
+        return "redirect:/displayMessages";
     }
 
 
