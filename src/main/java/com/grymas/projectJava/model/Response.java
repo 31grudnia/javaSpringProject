dbc386ec4b7db6aa704ad9bf5dc150a2389f45e9
31grudnia <mikolaj.starczewski@gmail.com>
2022-11-06 21:27:38 +0100

    ver. 0.5.1 - rest

---
+++ src/main/java/com/grymas/projectJava/model/Response.java
@@ -0,0 +1,15 @@
+package com.grymas.projectJava.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Response {
+
+    private String statusCode;
+    private String statusMsg;
+
+}
