8684b18670bdba03d6b8206f4e839079b1728738
31grudnia <mikolaj.starczewski@gmail.com>
2022-11-07 20:05:32 +0100

    ver. 0.5.2 - rest

--- src/main/java/com/grymas/projectJava/config/ProjectSecurityConfig.java
+++ src/main/java/com/grymas/projectJava/config/ProjectSecurityConfig.java
@@ -14,7 +14,7 @@ public class ProjectSecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf().ignoringAntMatchers("/saveMsg").ignoringAntMatchers("/public/**")
-                .ignoringAntMatchers("/api/**").and()
+                .ignoringAntMatchers("/api/**").ignoringAntMatchers("/data-api/**").and()
                 .authorizeRequests()
                 .mvcMatchers("/dashboard").authenticated()
                 .mvcMatchers("/displayProfile").authenticated()
@@ -23,6 +23,7 @@ public class ProjectSecurityConfig {
                 .mvcMatchers("/displayMessages").hasRole("ADMIN")
                 .mvcMatchers("/admin/**").hasRole("ADMIN")
                 .mvcMatchers("/api/**").authenticated()
+                .mvcMatchers("/data-api/**").authenticated()
                 .mvcMatchers("/home").permitAll()
                 .mvcMatchers("/holidays/**").permitAll()
                 .mvcMatchers("/contact").permitAll()
